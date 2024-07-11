e42192dcaaa566706925fb936193356d23573b1f
nihed <mbarek@nihed.com>
2024-07-11 14:30:52 +0400

    multi entries

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -17,6 +17,19 @@ func Hello(name string) (string, error) {
   return message,nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+   messages := make(map[string]string)
+
+   for _, name := range names {
+     message, err := Hello(name)
+     if err != nil {
+        return nil, err
+     }
+     messages[name] = message
+   }
+   return messages, nil
+}
+
 func randomFormat() string {
   formats := []string{
    "Hi, %v. Welcome!",
--- hello/hello.go
+++ hello/hello.go
@@ -10,12 +10,12 @@ func main() {
   log.SetPrefix("greetings: ")
   log.SetFlags(0)
 
-
-  message, err := greetings.Hello("Gladys")
+  names := []string{"Gladys","Samantha","Darrin"}
+  messages, err := greetings.Hellos(names)
   if err != nil {
     log.Fatal(err)
   }
 
-  fmt.Println(message)
+  fmt.Println(messages)
 
 }
